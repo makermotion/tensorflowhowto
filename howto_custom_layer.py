6a21abaf5b6b5f06d931cd86927b99ac9c911bf8
makermotion <muratcan.kurtulus@gmail.com>
2021-11-13 01:44:45 +0300

    styling

--- howto_custom_layer.py
+++ howto_custom_layer.py
@@ -9,15 +9,8 @@ class CustomDenseLayer(tf.keras.layers.Layer):
 	def build(self, input_shape):
 		w_init = tf.random_normal_initializer()
 		b_init = tf.zeros_initializer()
-		self.w = tf.Variable(name='kernel',
-							 initial_value=w_init(shape=(input_shape[-1], self.units), dtype=tf.float32),
-							 trainable=True,
-							)
-		self.b = tf.Variable(name='bias',
-							 initial_value=b_init(shape=(self.units,), dtype=tf.float32),
-							 trainable=True,
-							)
-							 
+		self.w = tf.Variable(name='kernel',initial_value=w_init(shape=(input_shape[-1], self.units), dtype=tf.float32), trainable=True)
+		self.b = tf.Variable(name='bias',initial_value=b_init(shape=(self.units,), dtype=tf.float32), trainable=True)
 	
 	def call(self, inputs):
 		return self.activtion(tf.matmul(inputs, self.w) + self.b)
