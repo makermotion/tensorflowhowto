77a9479270553fed1903b24ef38bb3d49c83ca0b
makermotion <muratcan.kurtulus@gmail.com>
2021-11-30 00:04:19 +0300

    add example

---
+++ howto_custom_loss.py
@@ -0,0 +1,12 @@
+import tensorflow as tf
+
+class CustomHuberLoss(tf.keras.losses.Loss):
+    def __init__(self, threshold):
+        self.threshold = threshold
+
+    def call(self, y_true, y_pred):
+        error = y_true - y_pred
+        is_small_error = tf.abs(error) <= self.threshold
+        small_error_loss = tf.square(error) / 2
+        big_error_loss = self.threshold * (tf.abs(error) - (0.5 * self.threshold))
+        return tf.where(is_small_error, small_error_loss, big_error_loss)
